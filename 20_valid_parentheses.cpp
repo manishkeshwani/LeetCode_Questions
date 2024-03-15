a9f3ddeb861d778cbda9321b52803c3d6ed80c5e
Manish Keshwani <113196913+manishkeshwani@users.noreply.github.com>
2024-03-15 21:23:26 +0530

    Add files via upload

---
+++ 20_valid_parentheses.cpp
@@ -0,0 +1,48 @@
+/*
+20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+https://leetcode.com/problems/valid-parentheses/
+
+*/
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> stk;
+        
+        for(char c : s){
+            if(c=='(' || c=='{' || c=='['){
+                stk.push(c);
+            }
+            else{
+                if(stk.empty() || 
+                    c == ')' && stk.top()!='(' ||
+                    c == '}' && stk.top()!='{' ||
+                    c == ']' && stk.top()!='['){
+                    return false;
+                }
+                stk.pop();
+            }
+        }        
+        return stk.empty();
+    }
+};
